28e58f003677b19ea4c05198e231347f28dee864
Yaakov <yaakov.smith@wisetechglobal.com>
2017-11-01 15:37:48 +1100

    Initial project

---
+++ CompressionMissingMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Exceptions;
+
+namespace CompressionMissingMethod
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Do something simple with Compression
+			try
+			{
+				ZipFile.OpenRead(Guid.NewGuid().ToString("N") + ".zip");
+			}
+			catch (FileNotFoundException)
+			{
+			}
+
+			// Do something simple with MSBuild
+			try
+			{
+				new Project().ToString();
+			}
+			catch (InvalidProjectFileException)
+			{
+			}
+
+			Console.WriteLine("Everything worked, or failed as expected.");
+		}
+	}
+}
